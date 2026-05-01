5009fd5a21235642f812523022317c692f150399
Victor Riddlestone Aguilar <victorriddlestoneaguilar@gmail.com>
2026-05-01 12:12:00 +0200

    fix: esint disable en useEffect de evaluate

--- web/frontend/app/evaluate/page.tsx
+++ web/frontend/app/evaluate/page.tsx
@@ -33,6 +33,7 @@ export default function EvaluatePage() {
     } catch {}
   };
 
+  // eslint-disable-next-line react-hooks/set-state-in-effect
   useEffect(() => { loadStatus(); }, []);
 
   const runEvaluate = () => {
